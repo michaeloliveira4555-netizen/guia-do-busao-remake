d637ca4634af7c4cfcf1b58eb7ea80a0e8ed4c30
michaeloliveira4555-netizen <michael.oliveira4555@gmail.com>
2026-08-29 17:09:53 -0300

    Revert "feat: implement backend service and frontend pages while initializing project dependencies"
    This reverts commit cd85cc8e3729c867466384bc29de5041aa11dc90.

--- guia-do-busao/backend/routes/usuariosRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-
-const usuariosController = require("../../Login/usuariosController");
-
-router.get("/", usuariosController.listarUsuarios);
-
-module.exports = router;
